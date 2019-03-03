bb299b38b57454bd938a01be4c15d7a72197bf9d
judaschrist <operaphantom@126.com>
2019-03-03 16:45:45 +0800

    popup page style

--- popup.js
+++ popup.js
@@ -1,10 +1,42 @@
-let changeColor = document.getElementById('changeColor');
+let mainDiv = document.getElementById('main');
 
-chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
+chrome.storage.local.get(null, function(result) {
+    Object.keys(result).forEach(function(paperId) {
+        console.log(paperId, result[paperId]);
+        let div = document.createElement('div');
+        div.innerHTML = result[paperId]['title'];
+        div.setAttribute('class', 'row');
+        mainDiv.appendChild(div);
+    });
+    // for (let i = 0; i < papers.length; i++) {
+    //     let title = papers[i].getElementsByClassName('gs_rt')[0].textContent;
+    //     let otherInfo = {title: title};
+    //     let paperId = papers[i].getAttribute('data-cid');
+    //     let button = null;
+    //     if (result[paperId] === undefined) {
+    //         button = document.createElement('button');
+    //         button.innerHTML = "Add to RW";
+    //         button.setAttribute("id", "btn_" + paperId);
+    //         button.addEventListener('click', onClickAddBtn(paperId, button, otherInfo));
+    //     } else {
+    //         button = document.createElement('button');
+    //         button.innerHTML = "Added";
+    //     }
+    //     let nodesToAdd = papers[i].getElementsByClassName('gs_fl');
+    //     nodesToAdd[nodesToAdd.length-1].appendChild(button);
+    //     // console.log(paperId + "----" + result[paperId]);
+    // }
+
 });
 
-changeColor.onclick = function(element) {
-    console.log('click!!!');
-};
+let delBtn = document.getElementById('delete-btn');
+delBtn.addEventListener('click', removeAll);
+
+function removeFromLib(paperId) {
+    chrome.storage.local.remove(paperId, null);
+}
+
+function removeAll() {
+    chrome.storage.local.clear();
+}
+
--- selectPaper.js
+++ selectPaper.js
@@ -5,60 +5,57 @@ chrome.storage.local.get(null, function(result) {
     console.log(papers.length + ' papers on the page');
     for (let i = 0; i < papers.length; i++) {
         let title = papers[i].getElementsByClassName('gs_rt')[0].textContent;
+        let otherInfo = {title: title};
         let paperId = papers[i].getAttribute('data-cid');
         let button = null;
         if (result[paperId] === undefined) {
             button = document.createElement('button');
             button.innerHTML = "Add to RW";
             button.setAttribute("id", "btn_" + paperId);
-            button.addEventListener('click', onClickAddBtn(paperId, button));
+            button.addEventListener('click', onClickAddBtn(paperId, button, otherInfo));
         } else {
             button = document.createElement('button');
             button.innerHTML = "Added";
         }
         let nodesToAdd = papers[i].getElementsByClassName('gs_fl');
         nodesToAdd[nodesToAdd.length-1].appendChild(button);
-        console.log(paperId + "----" + result[paperId]);
+        // console.log(paperId + "----" + result[paperId]);
     }
 
 });
 
-function onClickAddBtn(paperId, button) {
+function onClickAddBtn(paperId, button, otherInfo) {
     return function () {
-        addToLib(paperId);
+        addToLib(paperId, otherInfo);
         button.innerHTML = "Added";
         button.removeEventListener('click', onClickAddBtn);
     };
 }
 
-
-function removeFromLib(paperId) {
-    chrome.storage.local.remove(paperId, null);
-}
-
-function addToLib(paperId) {
+function addToLib(paperId, otherInfo) {
     chrome.storage.local.set({[paperId]: null}, function() {
-        addAbstract(paperId);
+        addAbstract(paperId, otherInfo);
     });
     return paperId;
 }
 
-function addAbstract(paperId) {
+function addAbstract(paperId, otherInfo) {
     let xhr = new XMLHttpRequest();
     let url = `https://scholar.google.com/scholar?q=info:${paperId}:scholar.google.com/&output=qabs&scirp=1&hl=en`;
-    xhr.onreadystatechange = readPaperDetail(xhr, paperId); // Implemented elsewhere.
+    xhr.onreadystatechange = readPaperDetail(xhr, paperId, otherInfo); // Implemented elsewhere.
     xhr.open("GET", url, true);
     // xhr.setRequestHeader("user-agent", HEADER);
     xhr.send();
 }
 
-function readPaperDetail(xhr, paperId) {
+function readPaperDetail(xhr, paperId, otherInfo) {
     return function () {
         if (xhr.readyState === 4) {
             let tempDiv = document.createElement('div');
             tempDiv.innerHTML = xhr.responseText.replace(/<script(.|\s)*?\/script>/g, '');
             let abstarct = tempDiv.getElementsByClassName("gs_qabs_snippet")[0].textContent;
-            chrome.storage.local.set({[paperId]: abstarct}, null);
+            otherInfo['abstract'] = abstarct;
+            chrome.storage.local.set({[paperId]: otherInfo}, null);
         }
     }
 }
